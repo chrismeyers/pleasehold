0b2c44e2432a78e75d2ea6fcfabfb5e995638d32
Chris Meyers <cm.02.93@gmail.com>
2019-02-15 19:52:14 -0700

    removed extra white space.

--- examples/basic.py
+++ examples/basic.py
@@ -4,7 +4,7 @@ import pleasehold
 if __name__ == '__main__':
     print('before')
     before = time.time()
-    
+
     with pleasehold.hold('starting', 'complete') as holding:
         time.sleep(2)
         holding.push('1')
@@ -14,12 +14,12 @@ if __name__ == '__main__':
         time.sleep(2)
         holding.push('another push')
         time.sleep(2)
-    
+
     with pleasehold.hold(begin_msg='props changed', delay=0.1, symbol='#') as holding:
         time.sleep(2)
         holding.push('pushed something')
         time.sleep(2)
-    
+
     # TODO: Handle stdin (sudo prompt). Pause loading?
-    
+
     print(f'time elapsed: {time.time() - before}')
