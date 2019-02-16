65870d145cc7442920590b7a0551d598841204c7
Chris Meyers <cm.02.93@gmail.com>
2019-02-15 19:53:53 -0700

    added additional properties to the PleaseHold class.

--- pleasehold/core.py
+++ pleasehold/core.py
@@ -6,13 +6,13 @@ class PleaseHold():
     def __init__(self, begin_msg='begin', end_msg='end', delay=1.0, symbol='.'):
         self._begin_msg = begin_msg
         self._end_msg = end_msg
+        self._delay = delay
+        self._symbol = symbol
         self._loading_thread = threading.Thread(name='loading', target=self._loading)
-        self._loading_delay = delay
-        self._loading_symbol = symbol
         self._loading_lock = threading.RLock()
-        self._event = threading.Event()
+        self._loading_event = threading.Event()
 
-    
+
     def __enter__(self):
         self.start()
         return self
@@ -23,44 +23,69 @@ class PleaseHold():
 
 
     @property
-    def loading_delay(self):
-        return self._loading_delay
-    
-        
-    @loading_delay.setter
-    def loading_delay(self, value):
-        self._loading_delay = value
-        
-    
+    def begin_msg(self):
+        return self._begin_msg
+
+
+    @begin_msg.setter
+    def begin_msg(self, value):
+        self._begin_msg = value
+
+
     @property
-    def loading_symbol(self):
-        return self._loading_symbol
-        
-        
-    @loading_symbol.setter
-    def loading_symbol(self, value):
-        self._loading_symbol = value
-        
-        
-    def start(self):
+    def end_msg(self):
+        return self._end_msg
+
+
+    @end_msg.setter
+    def end_msg(self, value):
+        self._end_msg = value
+
+
+    @property
+    def delay(self):
+        return self._delay
+
+
+    @delay.setter
+    def delay(self, value):
+        self._delay = value
+
+
+    @property
+    def symbol(self):
+        return self._symbol
+
+
+    @symbol.setter
+    def symbol(self, value):
+        self._symbol = value
+
+
+    def start(self, msg=None):
+        self._begin_msg = msg if msg is not None else self._begin_msg
+
         print(self._begin_msg, end='', flush=True)
-        
-        self._event.set()
-        
+
+        self._loading_event.set()
+
         if not self._loading_thread.is_alive():
             try:
                 self._loading_thread.start()
             except RuntimeError:
                 self._loading_thread = threading.Thread(name='loading', target=self._loading)
                 self._loading_thread.start()
-                
-        
-    def end(self):
-        self._event.clear()
+
+
+    def end(self, msg=None):
+        self._end_msg = msg if msg is not None else self._end_msg
+
+        self._loading_event.clear()
         self._loading_thread.join()
+
         print(self._end_msg, flush=True)
-        
-        
+
+
     def push(self, msg):
         with self._loading_lock:
             sys.stdout.write('\x1b[2K\r') # Clear the line
@@ -71,11 +96,11 @@ class PleaseHold():
 
 
     def _loading(self):
-        while self._event.is_set():
+        while self._loading_event.is_set():
             with self._loading_lock:
-                self._begin_msg += self._loading_symbol
-                print(self._loading_symbol, end='', flush=True)
-            time.sleep(self._loading_delay)
+                self._begin_msg += self._symbol
+                print(self._symbol, end='', flush=True)
+            time.sleep(self._delay)
 
 
 def hold(begin_msg='begin', end_msg='end', delay=1.0, symbol='.'):
